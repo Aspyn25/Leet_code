69d2c0dd5ff5b7fde0a4b18f9751f55e5beb82ba
Aspyn <jeongfree25@gmail.com>
2024-09-24 14:15:23 -0700

    69 Majority Element

---
+++ 69_Majority_Element.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def majorityElement(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        for x in nums:
+            count_num = nums.count(x)
+            if count_num > len(nums)/2:
+                return  x
+
+
+
+
+
+s = Solution()
+print(s.majorityElement([3,2,3]))
